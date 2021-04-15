dd813bb565767972f57bfaf545d9725f7aea5da7
Thomd209 <thomd209@gmail.com>
2021-04-16 02:28:36 +0300

    Add new file guess_capital_game.py and add modfied file README.md

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-Guess_capital is a game. It has the following rules:
+Guess_capital is the game. It has the following rules:
 1. There are two players: human (the way forward, this player will be called a player) and computer.
-2. The name of country randomly selected from the list is displayed in the terminal.
+2. The name of country randomly selected from the list and it displayed in the terminal.
+3. The program prompts the player to choose the correct answer from a list of capitals.
 3. The program asks the player to enter the name of the capital of this country.
-4. The player enters the name of the capital into the terminal. If this name is correct, the program informs the player that he has won and the player gets one point, else the program informs the player that he has lose and computer gets one point.
-5. The game will go on until one of the players gets 3 points.
+4. The player enters the name of the capital into the terminal. If this name is correct, the program informs the player that he gets one point, else the program informs the player that the computer gets one point.
+5. The game will go on until one of the players gets n points.
+6. The winner will be the player who scores n points.
---
+++ guess_capital_game.py
@@ -0,0 +1,133 @@
+from random import randint, shuffle
+
+# the list of dictionaries consist of countries and their capitals
+countries = [{'Afghanistan': 'Kabul'}, {'Albania': 'Tirana'}, {'Algeria': 'Algiers'}, {'Andorra': 'Andorra la Vella'},
+             {'Angola': 'Luanda'}, {'Antigua and Barbuda': 'Saint John\'s'}, {'Argentina': 'Buenos Aires'},
+             {'Armenia': 'Yerevan'}, {'Australia': 'Canberra'}, {'Austria': 'Vienna'}, {'Azerbaijan': 'Baku'},
+             {'Bahamas': 'Nassau'}, {'Bahrain': 'Manama'}, {'Bangladesh': 'Dhaka'}, {'Barbados': 'Bridgetown'},
+             {'Belarus': 'Minsk'}, {'Belgium': 'Brussels'}, {'Belize': 'Belmopan'}, {'Benin': 'Porto-Novo'},
+             {'Bhutan': 'Thimphu'}, {'Bolivia': 'La Paz'}, {'Bosnia and Herzegovina': 'Sarajevo'},
+             {'Botswana': 'Gaborone'}, {'Brazil': 'Brasilia'}, {'Brunei': 'Bandar Seri Begawan'}, {'Bulgaria': 'Sofia'},
+             {'Burkina Faso': 'Ouagadougou'}, {'Burundi': 'Gitega'}, {'Cabo Verde': 'Praia'},
+             {'Cambodia': 'Phnom Penh'}, {'Cameroon': 'Yaounde'}, {'Canada': 'Ottawa'},
+             {'Central African Republic': 'Bangui'}, {'Chad': 'N\'Djamena'}, {'Chile': 'Santiago'},
+             {'China': 'Beijing'}, {'Colombia': 'Bogota'}, {'Comoros': 'Moroni'},
+             {'Congo, Democratic Republic of the': 'Kinshasa'}, {'Congo, Republic of the': 'Brazzaville'},
+             {'Costa Rica': 'San Jose'}, {'Cote d\'Ivoire': 'Yamoussoukro'}, {'Croatia': 'Zagreb'}, {'Cuba': 'Havana'},
+             {'Cyprus': 'Nicosia'}, {'Czechia': 'Prague'}, {'Denmark': 'Copenhagen'}, {'Djibouti': 'Djibouti (city)'},
+             {'Dominica': 'Roseau'}, {'Dominican Republic': 'Santo Domingo'}, {'Ecuador': 'Quito'}, {'Egypt': 'Cairo'},
+             {'El Salvador': 'San Salvador'}, {'Equatorial Guinea': 'Oyala'}, {'Eritrea': 'Asmara'},
+             {'Estonia': 'Tallinn'}, {'Eswatini': 'Mbabane'}, {'Ethiopia': 'Addis Ababa'}, {'Fiji': 'Suva'},
+             {'Finland': 'Helsinki'}, {'France': 'Paris'}, {'Gabon': 'Libreville'}, {'Gambia': 'Banjul'},
+             {'Georgia': 'Tbilisi'}, {'Germany': 'Berlin'}, {'Ghana': 'Accra'}, {'Greece': 'Athens'},
+             {'Grenada': 'Saint George\'s'}, {'Guatemala': 'Guatemala City'}, {'Guinea': 'Conakry'},
+             {'Guinea-Bissau': 'Bissau'}, {'Guyana': 'Georgetown'}, {'Haiti': 'Port-au-Prince'},
+             {'Honduras': 'Tegucigalpa'}, {'Hungary': 'Budapest'}, {'Iceland': 'Reykjavik'}, {'India': 'New Delhi'},
+             {'Indonesia': 'Jakarta'}, {'Iran': 'Tehran'}, {'Iraq': 'Baghdad'}, {'Ireland': 'Dublin'},
+             {'Israel': 'Jerusalem'}, {'Italy': 'Rome'}, {'Jamaica': 'Kingston'}, {'Japan': 'Tokyo'},
+             {'Jordan': 'Amman'}, {'Kazakhstan': 'Nur-Sultan'}, {'Kenya': 'Nairobi'}, {'Kiribati': 'Tarawa'},
+             {'Kosovo': 'Pristina'}, {'Kuwait': 'Kuwait City'}, {'Kyrgyzstan': 'Bishkek'}, {'Laos': 'Vientiane'},
+             {'Latvia': 'Riga'}, {'Lebanon': 'Beirut'}, {'Lesotho': 'Maseru'}, {'Liberia': 'Monrovia'},
+             {'Libya': 'Tripoli'}, {'Liechtenstein': 'Vaduz'}, {'Lithuania': 'Vilnius'},
+             {'Luxembourg': 'Luxembourg (city)'}, {'Madagascar': 'Antananarivo'}, {'Malawi': 'Lilongwe'},
+             {'Malaysia': 'Kuala Lumpur'}, {'Maldives': 'Male'}, {'Mali': 'Bamako'}, {'Malta': 'Valletta'},
+             {'Marshall Islands': 'Majuro'}, {'Mauritania': 'Nouakchott'}, {'Mauritius': 'Port Louis'},
+             {'Mexico': 'Mexico City'}, {'Micronesia': 'Palikir'}, {'Moldova': 'Chisinau'}, {'Monaco': 'Monaco'},
+             {'Mongolia': 'Ulaanbaatar'}, {'Montenegro': 'Podgorica'}, {'Morocco': 'Rabat'}, {'Mozambique': 'Maputo'},
+             {'Myanmar': 'Naypyidaw'}, {'Namibia': 'Windhoek'}, {'Nauru': 'Yaren District'}, {'Nepal': 'Kathmandu'},
+             {'Netherlands': 'Amsterdam'}, {'New Zealand': 'Wellington'}, {'Nicaragua': 'Managua'}, {'Niger': 'Niamey'},
+             {'Nigeria': 'Abuja'}, {'North Korea': 'Pyongyang'}, {'North Macedonia': 'Skopje'}, {'Norway': 'Oslo'},
+             {'Oman': 'Muscat'}, {'Pakistan': 'Islamabad'}, {'Palau': 'Ngerulmud'}, {'Palestine': 'Jerusalem (East)'},
+             {'Panama': 'Panama City'}, {'Papua New Guinea': 'Port Moresby'}, {'Paraguay': 'Asuncion'},
+             {'Peru': 'Lima'}, {'Philippines': 'Manila'}, {'Poland': 'Warsaw'}, {'Portugal': 'Lisbon'},
+             {'Qatar': 'Doha'}, {'Romania': 'Bucharest'}, {'Russia': 'Moscow'}, {'Rwanda': 'Kigali'},
+             {'Saint Kitts and Nevis': 'Basseterre'}, {'Saint Lucia': 'Castries'},
+             {'Saint Vincent and the Grenadines': 'Kingstown'}, {'Samoa': 'Apia'}, {'San Marino': 'San Marino'},
+             {'Sao Tome and Principe': 'São Tomé'}, {'Saudi Arabia': 'Riyadh'}, {'Senegal': 'Dakar'},
+             {'Serbia': 'Belgrade'}, {'Seychelles': 'Victoria'}, {'Sierra Leone': 'Freetown'},
+             {'Singapore': 'Singapore'}, {'Slovakia': 'Bratislava'}, {'Slovenia': 'Ljubljana'},
+             {'Solomon Islands': 'Honiara'}, {'Somalia': 'Mogadishu'}, {'South Africa': 'Pretoria'},
+             {'South Korea': 'Seoul'}, {'South Sudan': 'Juba'}, {'Spain': 'Madrid'},
+             {'Sri Lanka': 'Sri Jayawardenepura Kotte'}, {'Sudan': 'Khartoum'}, {'Suriname': 'Paramaribo'},
+             {'Sweden': 'Stockholm'}, {'Switzerland': 'Bern'}, {'Syria': 'Damascus'}, {'Taiwan': 'Taipei'},
+             {'Tajikistan': 'Dushanbe'}, {'Tanzania': 'Dodoma'}, {'Thailand': 'Bangkok'}, {'Timor-Leste': 'Dili'},
+             {'Togo': 'Lomé'}, {'Tonga': 'Nukuʻalofa'}, {'Trinidad and Tobago': 'Port of Spain'}, {'Tunisia': 'Tunis'},
+             {'Turkey': 'Ankara'}, {'Turkmenistan': 'Ashgabat'}, {'Tuvalu': 'Funafuti'}, {'Uganda': 'Kampala'},
+             {'Ukraine': 'Kyiv'}, {'United Arab Emirates': 'Abu Dhabi'}, {'United Kingdom': 'London'},
+             {'United States of America': 'Washington, D.C.'}, {'Uruguay': 'Montevideo'}, {'Uzbekistan': 'Tashkent'},
+             {'Vanuatu': 'Port Vila'}, {'Vatican City (Holy See)': 'Vatican City'}, {'Venezuela': 'Caracas'},
+             {'Vietnam': 'Hanoi'}, {'Yemen': 'Sana\'a'}, {'Zambia': 'Lusaka'}, {'Zimbabwe': 'Harare'}]
+
+right_country = 0  # The country whose capital is the right answer
+first_wrong_country = 0  # The country whose capital is the wrong answer
+second_wrong_country = 0  # The country whose capital is the wrong answer
+third_wrong_country = 0  # The country whose capital is the wrong answer
+random_country = ''  # The country whose capital is the right answer and that will be show in the terminal
+right_capital = ''  # The capital that is the right answer
+first_wrong_capital = ''  # The country whose capital is the wrong answer
+second_wrong_capital = ''  # The country whose capital is the wrong answer
+third_wrong_capital = ''  # The country whose capital is the wrong answer
+player_points = 0  # Quantity of points of the player
+computer_points = 0  # Quantity of points of the computer
+# The number of points up to which the game is played
+while player_points < 3 and computer_points < 3:
+    right_country = randint(0, len(countries) - 1)
+    first_wrong_country = randint(0, len(countries) - 1)
+    second_wrong_country = randint(0, len(countries) - 1)
+    third_wrong_country = randint(0, len(countries) - 1)
+    # Extraction the country and capital from list item
+    for key, value in countries[right_country].items():
+        random_country = key
+        right_capital = value
+
+    # Extraction the wrong capital from list item
+    for value in countries[first_wrong_country].values():
+        first_wrong_capital = value
+
+    # Extraction the wrong capital from list item
+    for value in countries[second_wrong_country].values():
+        second_wrong_capital = value
+
+    # Extraction the wrong capital from list item
+    for value in countries[third_wrong_country].values():
+        third_wrong_capital = value
+
+    # Answer options
+    possible_answers = [right_capital, first_wrong_capital, second_wrong_capital, third_wrong_capital]
+    # Shuffling the list randomly
+    shuffle(possible_answers)
+    # Output to terminal the country whose capital is the right answer
+    print('The country:', random_country)
+    print('Choose the right option:')
+    # Output to terminal the list of mixed answers
+    print(', '.join(possible_answers))
+    # Inviting the user to enter the name of the capital
+    player_answer = input('Input a capital of this country: ')
+
+    # Function that checks the player answer
+    def check_answer(answer):
+        for capital in countries[right_country].values():
+            if answer == capital:
+                return True
+            else:
+                return False
+
+    # Checking the user answer
+    if check_answer(player_answer):
+        print('Right')
+        player_points += 1
+    else:
+        print('Wrong')
+        computer_points += 1
+
+    # Output the quantity of points of player and computer
+    print(f'Player: {player_points}. Computer: {computer_points}')
+
+    # Delete the country whose capital is the right answer
+    del countries[right_country]
+
+# Winner determination
+if player_points > computer_points:
+    print("You win!")
+elif computer_points > player_points:
+    print("You lose")
